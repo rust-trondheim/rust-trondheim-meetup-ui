e538eaa3ec8160415897841ed86a664e4d84d4a9
Christian Brevik <christian.brevik@gmail.com>
2024-08-26 14:51:45 +0200

    feat: Basic GET issue title

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use slint::SharedString;
+use serde::{Serialize, Deserialize};
+
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
@@ -11,5 +14,39 @@ fn main() -> Result<(), slint::PlatformError> {
         }
     });
 
+    ui.on_request_first_issue_title({
+        let ui_handle = ui.as_weak();
+        move || {
+            let title = get_first_issue_title();
+            let ui = ui_handle.unwrap();
+            ui.set_first_issue_title(SharedString::from(title));
+        }
+    });
+
     ui.run()
+}
+
+fn get_first_issue_title() -> String {
+    let client = reqwest::blocking::Client::new();
+    
+    let body = client
+    .get("https://api.github.com/repos/Rust-Trondheim/.github/issues")
+    .header("User-Agent", "Rust-Trondheim-Meetup-UI")
+    .send()
+    .unwrap()
+    .text()
+    .unwrap();
+
+    let issue: Vec<Issue> = serde_json::from_str(&body).unwrap();
+
+    issue.first().unwrap().title.clone()
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Issue {
+    pub id: u64,
+    pub node_id: String,
+    pub url: String,
+    pub number: u64,
+    pub title: String,
 }
