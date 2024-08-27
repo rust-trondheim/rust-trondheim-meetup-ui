f557458e1675642ee42127447a978fecb7062f08
Christian Brevik <christian.brevik@gmail.com>
2024-08-27 14:33:35 +0200

    feat: Rewrite to use Octocrab for Github API

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use slint::SharedString;
-use serde::{Serialize, Deserialize};
+use octocrab::*;
+use tokio::runtime::Runtime;
 
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
+    let tokio_runtime = Runtime::new().unwrap();
 
     ui.on_request_increase_value({
         let ui_handle = ui.as_weak();
@@ -17,36 +19,19 @@ fn main() -> Result<(), slint::PlatformError> {
     ui.on_request_first_issue_title({
         let ui_handle = ui.as_weak();
         move || {
-            let title = get_first_issue_title();
             let ui = ui_handle.unwrap();
-            ui.set_first_issue_title(SharedString::from(title));
+            tokio_runtime.block_on(async move {
+                let title = get_first_issue_title().await;
+                ui.set_first_issue_title(SharedString::from(title));
+            });
         }
     });
 
     ui.run()
 }
 
-fn get_first_issue_title() -> String {
-    let client = reqwest::blocking::Client::new();
-    
-    let body = client
-    .get("https://api.github.com/repos/Rust-Trondheim/.github/issues")
-    .header("User-Agent", "Rust-Trondheim-Meetup-UI")
-    .send()
-    .unwrap()
-    .text()
-    .unwrap();
-
-    let issue: Vec<Issue> = serde_json::from_str(&body).unwrap();
-
-    issue.first().unwrap().title.clone()
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Issue {
-    pub id: u64,
-    pub node_id: String,
-    pub url: String,
-    pub number: u64,
-    pub title: String,
+async fn get_first_issue_title() -> String {
+    let client = octocrab::instance();
+    let issue = client.issues("Rust-Trondheim", ".github").get(1).await.unwrap();
+    issue.title
 }
