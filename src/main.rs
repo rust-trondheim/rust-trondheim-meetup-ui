1cd4c63c3793b2d869427dd76e83920effe35e6a
Christian Brevik <christian.brevik@gmail.com>
2024-08-27 20:25:41 +0200

    feat(mob): List issues in UI
    Co-authored-by: Henrik Hørlück Berg <henrik@horluck.no>
    Co-authored-by: Eirik Arthur Blekesaune <eirik.blekesaune@gmail.com>
    Co-authored-by: Johannes Røsvik <j.rosvik@gmail.com>
    Co-authored-by: Sigvart Hovland <sigvart.m@gmail.com>
    Co-authored-by: Thomas Løkkeborg <thomas.lokkeborg@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use slint::SharedString;
+use slint::{ModelRc, SharedString, VecModel};
 use octocrab::*;
 use tokio::runtime::Runtime;
 
@@ -6,7 +6,7 @@ slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
-    let tokio_runtime = Runtime::new().unwrap();
+    let tokio_runtime: &'static Runtime = Box::leak(Box::new(Runtime::new().unwrap()));
 
     ui.on_request_increase_value({
         let ui_handle = ui.as_weak();
@@ -16,22 +16,22 @@ fn main() -> Result<(), slint::PlatformError> {
         }
     });
 
-    ui.on_request_first_issue_title({
-        let ui_handle = ui.as_weak();
-        move || {
-            let ui = ui_handle.unwrap();
-            tokio_runtime.block_on(async move {
-                let title = get_first_issue_title().await;
-                ui.set_first_issue_title(SharedString::from(title));
-            });
-        }
-    });
+        ui.on_request_first_issue_title({
+            let ui_handle = ui.as_weak();
+            move || {
+                let ui = ui_handle.unwrap();
+                slint::spawn_local(async move {
+                        let titles = tokio_runtime.spawn(get_first_issue_title()).await.unwrap();
+                        ui.set_issue_titles(ModelRc::new(VecModel::from(titles)));
+                    }).unwrap();
+            }
+        });
 
     ui.run()
 }
 
-async fn get_first_issue_title() -> String {
+async fn get_first_issue_title() -> Vec<SharedString> {
     let client = octocrab::instance();
-    let issue = client.issues("Rust-Trondheim", ".github").get(1).await.unwrap();
-    issue.title
+    let issues = client.issues("Rust-Trondheim", ".github").list().per_page(10).send().await.unwrap();
+    issues.into_iter().map(|i| i.title.into()).collect()
 }
